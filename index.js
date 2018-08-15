30b01f150aaa4ae2a486fbd7f3ec045b635d5e8d
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-15 08:43:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function printBadges(name_array) {
+  for (let i = 0; i < name_array.length; i++) {
+    console.log(`Welcome ${name_array[i]}! You are employee #${i + 1}.`);
+  }
+  return name_array
+}
+
+function tailsNeverFails() {
+  let tails_count = 0
+  while (maybeTrue()) {
+    tails_count++
+  }
+  return `You got ${tails_count} tails in a row!`
+}
+function getRandom() {
+  return Math.random();
+}
+
+function maybeTrue () {
+  return Math.random() >= 0.5;
+}
